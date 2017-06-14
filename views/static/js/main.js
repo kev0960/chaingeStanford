abcb5366f7624ee660d227606ad1de3876c432df
Se Won Jang <swjang@stanford.edu>
2017-06-14 03:21:45 -0700

    sdfdsf

--- views/static/js/main.js
+++ views/static/js/main.js
@@ -192,7 +192,7 @@ const toggle_progress = function(txn_type) {
 
 		setTimeout(function() {
 			percentage++;
-			console.log(precentage);
+			console.log(percentage);
 			$('#progress_data')[0].style.width = percentage + "%";
 		}, 200);
 	}
